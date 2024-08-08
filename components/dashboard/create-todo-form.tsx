eee5eb0fd0821412e0cf129688644ad33f8c0807
Guitt0r <andriyka35@gmail.com>
2024-08-08 03:29:08 +0300

    feat: add UI components for todo logic

---
+++ components/dashboard/create-todo-form.tsx
@@ -0,0 +1,124 @@
+"use client";
+
+import { z } from "zod";
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { useState, useTransition } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
+import { DialogClose } from "@/components/ui/dialog";
+import { FormError } from "@/components/form-error";
+
+import { TodoSchema } from "@/schemas";
+import { createTodo } from "@/actions/todo";
+
+type Props = {
+  onSuccess: () => void;
+};
+
+export const CreateTodoForm = ({ onSuccess }: Props) => {
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | undefined>();
+
+  const form = useForm<z.infer<typeof TodoSchema>>({
+    resolver: zodResolver(TodoSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      isCompleted: false,
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof TodoSchema>) => {
+    startTransition(() => {
+      createTodo(values).then((res) => {
+        setError(res.error);
+        if (res.success) {
+          toast.success(res.success);
+          onSuccess();
+        }
+      });
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <div className="space-y-4">
+          <FormField
+            disabled={isPending}
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input
+                    className="text-xl font-semibold"
+                    {...field}
+                    placeholder="e.g. Feed the cat, Water plants "
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            disabled={isPending}
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className="text-base text-justify"
+                    rows={10}
+                    {...field}
+                    placeholder="Add a description for your todo."
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            disabled={isPending}
+            control={form.control}
+            name="isCompleted"
+            render={({ field }) => (
+              <FormItem className="flex flex-row gap-x-2 items-end ml-1">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <FormLabel>Completed</FormLabel>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormError message={error} />
+        <DialogClose asChild>
+          <Button disabled={isPending} type="submit" className="w-full">
+            Create
+          </Button>
+        </DialogClose>
+      </form>
+    </Form>
+  );
+};
---
+++ hooks/use-confirm.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export const useConfirm = (
+  title: string,
+  message: string,
+  {
+    confirmButtonLabel,
+    confirmButtonVariant,
+  }: {
+    confirmButtonLabel?: string;
+    confirmButtonVariant?:
+      | "link"
+      | "default"
+      | "destructive"
+      | "outline"
+      | "secondary"
+      | "ghost"
+      | null
+      | undefined;
+  } = { confirmButtonLabel: "Confirm" }
+): [() => JSX.Element, () => Promise<unknown>] => {
+  const [promise, setPromise] = useState<{
+    resolve: (value: boolean) => void;
+  } | null>(null);
+
+  const confirm = () =>
+    new Promise((resolve, reject) => {
+      setPromise({ resolve });
+    });
+
+  const handleClose = () => {
+    setPromise(null);
+  };
+
+  const handleConfirm = () => {
+    promise?.resolve(true);
+    handleClose();
+  };
+
+  const handleCancel = () => {
+    promise?.resolve(false);
+    handleClose();
+  };
+
+  // You could replace the Dialog with your library's version
+  const ConfirmationDialog = () => (
+    <Dialog open={promise !== null}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{message}</DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="pt-2">
+          <Button onClick={handleCancel} variant="outline">
+            Close
+          </Button>
+          <Button variant={confirmButtonVariant} onClick={handleConfirm}>
+            {confirmButtonLabel}
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+  return [ConfirmationDialog, confirm];
+};
